b6e58d0746e6fc50649f5a5730753e03519653ed
Thodorissio <thodorissiozos@gmail.com>
2023-01-15 22:32:26 +0200

    major code restructuring and updates

--- src/embedder.py
+++ src/embedder.py
@@ -3,7 +3,7 @@ import numpy as np
 from typing import List
 import os
 
-palmtree_path = "C:/Users/thodo/Documents/sxoli/diplomatiki/PalmTree/pre-trained-model/pre_trained_model"
+palmtree_path = os.getenv("PALMTREE_PATH")
 
 from pre_trained_palmtree import eval_utils as utils
 
@@ -11,8 +11,8 @@ from pre_trained_palmtree import eval_utils as utils
 def encode(text: List[str]) -> np.ndarray:
 
     palmtree = utils.UsableTransformer(
-        model_path=f"{palmtree_path}/palmtree/transformer.ep19",
-        vocab_path=f"{palmtree_path}/palmtree/vocab",
+        model_path=f"{palmtree_path}/pre_trained_palmtree/palmtree/transformer.ep19",
+        vocab_path=f"{palmtree_path}/pre_trained_palmtree/palmtree/vocab",
     )
     embeddings = palmtree.encode(text)
 
